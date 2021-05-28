5facba1879527d1fee6c5c852ff28be81e7178d2
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-05-28 07:46:43 -0300

    Adding JPA anottations and testing with h2(database)

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -1,12 +1,22 @@
 package com.example.demo.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
 // Serializable, pois quero que este objeto seja transformado em uma cadeia de bytes.
+// Anotações JPA (Entity)
+// Sempre damos preferência para especificação, e não implementação.
+@Entity
 public class User implements Serializable
 {
     private static final long serialVersionUID = 8256151461796770037L;
+    // definindo chave PK do bd
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
